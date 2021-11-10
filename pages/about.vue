67e3b5e6a606e0fd3dc9146dcc4a4782beae1fad
Drew Dru <drew.drux@gmail.com>
2021-11-11 01:34:33 +0300

    use module translation files

--- pages/about.vue
+++ pages/about.vue
@@ -1,6 +1,12 @@
+
+<i18n src="./about_en.json"></i18n>
+<i18n src="./about_ru.json"></i18n>
+
 <template>
   <div>
-    About
+    {{t('About')}}
+    <button @click="locale='ru'">ru</button>
+    <button @click="locale='en'">en</button>
     <div>
       {{ data }}
       <button :disabled="pending" @click="refresh">
@@ -19,6 +25,9 @@ export default {
 </script>
 
 <script lang="ts" setup>
+import { useI18n } from 'vue-i18n'
+const { t, locale } = useI18n()
+
 const { data, refresh, pending } = await useAsyncData('/api/hello', () => $fetch('/api/helloworld'))
 // TODO: check why doesn't work this:
 // const { data, refresh, pending } = await useFetch('/api/hello')
